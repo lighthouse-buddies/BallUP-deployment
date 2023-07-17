50daa74a045fe40dc427f4fbaf8b6ad51ea109ca
monki1 <114178697+monki1@users.noreply.github.com>
2023-07-17 17:45:59 -0400

    added createUser

--- pages/[loc]/index.jsx
+++ pages/[loc]/index.jsx
@@ -1,66 +1,69 @@
-import { getServerSession } from "next-auth/next"
-import { useSession } from "next-auth/react"
-import { prisma } from "../../src/prisma"
-import { useState } from "react"
-import  Image  from "next/image"
-import Head from 'next/head'
-import { ArrowPathIcon } from "@heroicons/react/24/outline"
-import Layout from "../../src/components/layout"
-import Places from "../../src/components/courtForm"
-import { authOptions } from "../api/auth/[...nextauth]"
+import { getServerSession } from "next-auth/next";
+import { useSession } from "next-auth/react";
+import { prisma } from "../../src/prisma";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Head from 'next/head';
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import Layout from "../../src/components/layout";
+import Places from "../../src/components/courtForm";
+import { authOptions } from "../api/auth/[...nextauth]";
+import axios from "axios";
 
 export default function Home({ markers, locArray }) {
-    const { data: session, status } = useSession()
-    
-    const [loaded, setLoaded] = useState(false);
-    
-    // // OFFLINE DEV
-    // const session = {
-    //   expires: "1",
-    //   user: { email: "a", name: "Delta", image: "c" },
-    // }
+  const { data: session, status } = useSession();
+  const [loaded, setLoaded] = useState(false);
 
-    // const locArray = loc.split(",")
+  useEffect(() => {
+    const createUser = async () => {
+      if (session?.user?.email) {
+        const response = await axios.put('/api/db/createUser', {
+          email: session.user.email
+        });
+      }
+    };
 
-    return (
-    <>
-      <Head>
-        <title>BallUp - Claim your court</title>
-        <meta name="description" content="Suggest your own court" />
-        <meta property="og:title" content="Court"/>
-        <meta property="og:description" content="Suggest your own court"/>
-        <link rel="icon" href="/ball_up.svg" />
-      </Head>
-      
-      {!loaded && <div className="flex h-screen">
-        <div className="m-auto">
-        <ArrowPathIcon className="h-10 w-10 mx-auto animate-spin text-gray-500"/>
-        </div>
-      </div>}
+    createUser();
+  }, [session]);
 
-      <Layout 
-      main={        
-        <div>
-          <Places markers={markers} session={session} locArray={locArray} setLoaded={setLoaded}/>
-        </div>
-      }/>
-      
-    </>
-  )
+  return (
+      <>
+        <Head>
+          <title>BallUp - Claim your court</title>
+          <meta name="description" content="Suggest your own court" />
+          <meta property="og:title" content="Court" />
+          <meta property="og:description" content="Suggest your own court" />
+          <link rel="icon" href="/ball_up.svg" />
+        </Head>
+
+        {!loaded && (
+            <div className="flex h-screen">
+              <div className="m-auto">
+                <ArrowPathIcon className="h-10 w-10 mx-auto animate-spin text-gray-500" />
+              </div>
+            </div>
+        )}
+
+        <Layout
+            main={
+              <div>
+                <Places markers={markers} session={session} locArray={locArray} setLoaded={setLoaded} />
+              </div>
+            }
+        />
+      </>
+  );
 }
 
 export async function getServerSideProps(context) {
-  const session = await getServerSession(context.req, context.res, authOptions)
-  
+  const session = await getServerSession(context.req, context.res, authOptions);
+
   // Quick check that query contains valid location
   const loc = context.query.loc;
-  const locArray = loc.split(",")
+  const locArray = loc.split(",");
   if (locArray.length !== 3 || isNaN(locArray[0]) || isNaN(locArray[1]) || isNaN(locArray[2])) {
-    return { notFound: true }
+    return { notFound: true };
   }
-
-  // // OFFLINE DEV
-  // const markers = []
 
   const markers = await prisma.courts.findMany();
 
@@ -70,5 +73,5 @@ export async function getServerSideProps(context) {
       markers: JSON.parse(JSON.stringify(markers)),
       locArray: JSON.parse(JSON.stringify(locArray)),
     },
-  }
+  };
 }
---
+++ pages/api/db/createUser.jsx
@@ -0,0 +1,44 @@
+import { prisma } from "../../../src/prisma";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+
+export default async function createUser(req, res) {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+        res.status(401).send("No permissions");
+        return;
+    }
+
+    const { email } = req.body;
+    if (!email) {
+        res.status(400).send("Email is required");
+        return;
+    }
+
+    try {
+        // Check if a user with the same email already exists
+        const count = await prisma.users.count({
+            where: { email: email },
+        });
+
+        if (count > 0) {
+            res.status(200).send("Success");
+            return;
+        }
+
+        // Create a new user if no user with the same email exists
+        const user = await prisma.users.create({
+            data: {
+                email: email,
+                name: email,
+            },
+        });
+
+        // Handle success
+        res.status(200).send("Success");
+    } catch (error) {
+        // Handle error
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
